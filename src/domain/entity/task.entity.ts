3fda0e19a56c0b3284e0f65361d1977eabb193f2
Almir Toledo <deendsstudios@gmail.com>
2023-07-12 22:05:29 -0300

    feat: add Task entity

---
+++ src/domain/entity/task.entity.ts
@@ -0,0 +1,10 @@
+import { TaskStatus } from "../enums/task-status.enum";
+
+export interface Task {
+  id: string;
+  status: TaskStatus;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
